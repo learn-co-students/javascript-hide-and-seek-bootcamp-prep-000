aece6a1a57dbae0dfb375dfa1658edf6cff7688d
pletcher <charles.pletcher@gmail.com>
2016-06-15 22:20:19 -0400

    Remove fixRankedList()

--- index.js
+++ index.js
@@ -29,29 +29,3 @@ function increaseRankBy(n) {
     }
   }
 }
-
-function fixRankedList() {
-  const rankedLists = document.querySelectorAll('.ranked-list')
-
-  for (let i = 0, l = rankedLists.length; i < l; i++) {
-    let children = rankedLists[i].children
-
-    let numbers = []
-    let inOrder = true
-    for (let j = 0, k = children.length; j < k; j++) {
-      let number = parseInt(children[j].innerHTML)
-
-      if (number < numbers[j - 1]) {
-        inOrder = false
-      }
-
-      numbers.push(number)
-    }
-
-    if (!inOrder) {
-      for (let j = 0, end = children.length - 1; end > -1; end--, j++) {
-        children[j].innerHTML = numbers[end]
-      }
-    }
-  }
-}
