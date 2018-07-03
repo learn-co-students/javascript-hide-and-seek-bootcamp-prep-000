4f4b63463818323a1bb75f5919e71cdfa12b1d8e
maxwell <github email address>
2018-07-03 11:02:39 -0400

    updated test from expect to chai

--- test/index-test.js
+++ test/index-test.js
@@ -6,39 +6,39 @@ describe('index', () => {
     })
   })
 
-  // describe('nestedTarget()', () => {
-  //   it('pulls a .target out of #nested', () => {
-  //     expect(nestedTarget()).toEqual(document.querySelector('#nested .target'))
-  //   })
-  // })
-  //
-  // describe('deepestChild()', () => {
-  //   it('returns the most deeply nested child in #grand-node', () => {
-  //     console.log(deepestChild().innerHTML)
-  //     expect(deepestChild()).toBe(document.querySelector('#grand-node div div div div'))
-  //   })
-  // })
-  //
-  // describe('increaseRankBy(n)', () => {
-  //   it('increases ranks in .ranked-list by n', () => {
-  //     increaseRankBy(3)
-  //
-  //     const rankedLists = document.querySelectorAll('.ranked-list')
-  //     const firstList = rankedLists[0]
-  //     const secondList = rankedLists[1]
-  //
-  //     let children = firstList.children
-  //     let start = 1
-  //     for (let i = 0, l = children.length; i < l; i++) {
-  //       expect(parseInt(children[i].innerHTML)).toEqual(start + i + 3)
-  //     }
-  //
-  //     children = secondList.children
-  //     start = 12
-  //
-  //     for (let i = 0, l = children.length; i < l; i++) {
-  //       expect(parseInt(children[i].innerHTML)).toEqual(start - i + 3)
-  //     }
-  //   })
-  // })
+  describe('nestedTarget()', () => {
+    it('pulls a .target out of #nested', () => {
+      expect(nestedTarget()).to.equal(document.querySelector('#nested .target'))
+    })
+  })
+
+  describe('deepestChild()', () => {
+    it('returns the most deeply nested child in #grand-node', () => {
+      console.log(deepestChild().innerHTML)
+      expect(deepestChild()).to.equal(document.querySelector('#grand-node div div div div'))
+    })
+  })
+
+  describe('increaseRankBy(n)', () => {
+    it('increases ranks in .ranked-list by n', () => {
+      increaseRankBy(3)
+
+      const rankedLists = document.querySelectorAll('.ranked-list')
+      const firstList = rankedLists[0]
+      const secondList = rankedLists[1]
+
+      let children = firstList.children
+      let start = 1
+      for (let i = 0, l = children.length; i < l; i++) {
+        expect(parseInt(children[i].innerHTML)).to.equal(start + i + 3)
+      }
+
+      children = secondList.children
+      start = 12
+
+      for (let i = 0, l = children.length; i < l; i++) {
+        expect(parseInt(children[i].innerHTML)).to.equal(start - i + 3)
+      }
+    })
+  })
 })
