3847dc257df85b27c3d69a847a2c192d11cdc1c0
maxwell <github email address>
2018-07-03 10:58:40 -0400

    updating test framework

--- test/index-test.js
+++ test/index-test.js
@@ -1,44 +1,44 @@
 describe('index', () => {
   describe('getFirstSelector(selector)', () => {
     it('returns the first element that matches the selector', () => {
-      expect(getFirstSelector('div').id).toEqual('nested')
-      expect(getFirstSelector('.ranked-list')).toEqual(document.querySelector('.ranked-list'))
+      expect(getFirstSelector('div').id).to.equal('nested')
+      expect(getFirstSelector('.ranked-list')).to.equal(document.querySelector('.ranked-list'))
     })
   })
 
-  describe('nestedTarget()', () => {
-    it('pulls a .target out of #nested', () => {
-      expect(nestedTarget()).toEqual(document.querySelector('#nested .target'))
-    })
-  })
-
-  describe('deepestChild()', () => {
-    it('returns the most deeply nested child in #grand-node', () => {
-      console.log(deepestChild().innerHTML)
-      expect(deepestChild()).toBe(document.querySelector('#grand-node div div div div'))
-    })
-  })
-
-  describe('increaseRankBy(n)', () => {
-    it('increases ranks in .ranked-list by n', () => {
-      increaseRankBy(3)
-
-      const rankedLists = document.querySelectorAll('.ranked-list')
-      const firstList = rankedLists[0]
-      const secondList = rankedLists[1]
-
-      let children = firstList.children
-      let start = 1
-      for (let i = 0, l = children.length; i < l; i++) {
-        expect(parseInt(children[i].innerHTML)).toEqual(start + i + 3)
-      }
-
-      children = secondList.children
-      start = 12
-
-      for (let i = 0, l = children.length; i < l; i++) {
-        expect(parseInt(children[i].innerHTML)).toEqual(start - i + 3)
-      }
-    })
-  })
+  // describe('nestedTarget()', () => {
+  //   it('pulls a .target out of #nested', () => {
+  //     expect(nestedTarget()).toEqual(document.querySelector('#nested .target'))
+  //   })
+  // })
+  //
+  // describe('deepestChild()', () => {
+  //   it('returns the most deeply nested child in #grand-node', () => {
+  //     console.log(deepestChild().innerHTML)
+  //     expect(deepestChild()).toBe(document.querySelector('#grand-node div div div div'))
+  //   })
+  // })
+  //
+  // describe('increaseRankBy(n)', () => {
+  //   it('increases ranks in .ranked-list by n', () => {
+  //     increaseRankBy(3)
+  //
+  //     const rankedLists = document.querySelectorAll('.ranked-list')
+  //     const firstList = rankedLists[0]
+  //     const secondList = rankedLists[1]
+  //
+  //     let children = firstList.children
+  //     let start = 1
+  //     for (let i = 0, l = children.length; i < l; i++) {
+  //       expect(parseInt(children[i].innerHTML)).toEqual(start + i + 3)
+  //     }
+  //
+  //     children = secondList.children
+  //     start = 12
+  //
+  //     for (let i = 0, l = children.length; i < l; i++) {
+  //       expect(parseInt(children[i].innerHTML)).toEqual(start - i + 3)
+  //     }
+  //   })
+  // })
 })
